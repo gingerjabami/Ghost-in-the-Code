79a68704e6c0f23ca08dfd1d73ca1160548b4dc3
Aditi Sikder <160914569+gingerjabami@users.noreply.github.com>
2026-01-30 23:03:51 +0530

    Time: 40 ms (40.5%), Space: 284.7 MB (31.1%) - LeetHub

--- 3809-best-reachable-tower/3809-best-reachable-tower.cpp
+++ 3809-best-reachable-tower/3809-best-reachable-tower.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-static bool chan(const pair<int, pair<int,int>>& a,
-                 const pair<int, pair<int,int>>& b){
-    if(a.first!=b.first){
-        return a.first > b.first;
-  }
-  if(a.second.first != b.second.first){
-    return a.second.first < b.second.first;
-  }
-  return a.second.second < b.second.second;
-}
-
     vector<int> bestTower(vector<vector<int>>& towers, vector<int>& center, int radius) {
         int n = towers.size();
         int cx= center[0];
         int cy= center[1];
-        vector<pair<int, pair<int,int>>> v;
+        int bestquality =0;
+        int x= 0;
+        int y= 0;
+        int q= 0;
+       vector<int> result = {INT_MAX, INT_MAX};
         for(int i=0; i<n ; i++){
-            int x= towers[i][0];
-            int y= towers[i][1];
-            int q= towers[i][2];
+             x= towers[i][0];
+             y= towers[i][1];
+             q= towers[i][2];
 
             int d = abs(cx - x) + abs(cy - y);
             if(d<= radius){
-                v.push_back({q,{x,y}});
+            if ( q > bestquality ){
+                bestquality =q;
+                result = {x,y};
+            }
+            else if( q == bestquality){
+                result = min(result, {x,y});
+            } 
             }
         }
-if(v.empty()){
-    return{-1,-1};
-}
-        sort(v.begin(), v.end(), chan);
+if(result[0] == INT_MAX) return {-1, -1};
 
-        return {v[0].second.first, v[0].second.second};
+return result;
+
+
     }
 };
